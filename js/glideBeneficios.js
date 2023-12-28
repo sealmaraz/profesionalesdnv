f420aac68d794943ee6262c1ffb4fef2fe99b1b5
Susana Elizabeth Almaraz <sealmaraz@yahoo.com.ar>
2023-12-28 00:24:46 -0300

    [FEAT] Revisión en dispositivo movil

--- js/glideBeneficios.js
+++ js/glideBeneficios.js
@@ -1,6 +1,16 @@
+let anchoPantalla = document.documentElement.clientWidth;
+if (anchoPantalla < 1024) {
+    new Glide('.glide', {
+        type: 'carousel',
+        perView: 1,
+        gap: 20,
+        startAt: 0,
+    }).mount();
+} else{
     new Glide('.glide', {
         type: 'carousel',
         perView: 3,
         gap: 30,
         startAt: 0,
     }).mount();
+}
