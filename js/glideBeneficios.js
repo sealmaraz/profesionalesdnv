20038fe775ffaf6c0b67c57b032a57fc24f005cb
Susana Elizabeth Almaraz <sealmaraz@yahoo.com.ar>
2023-12-28 01:14:52 -0300

    [FEAT] Revisión en dispositivo movil con js

--- js/glideBeneficios.js
+++ js/glideBeneficios.js
@@ -1,12 +1,20 @@
 let anchoPantalla = document.documentElement.clientWidth;
-if (anchoPantalla < 1024) {
+if (anchoPantalla < 430) {
     new Glide('.glide', {
         type: 'carousel',
         perView: 1,
         gap: 20,
         startAt: 0,
     }).mount();
-} else{
+} else 
+if (anchoPantalla > 431 && anchoPantalla < 768){
+    new Glide('.glide', {
+        type: 'carousel',
+        perView: 2,
+        gap: 30,
+        startAt: 0,
+    }).mount();
+} else if (anchoPantalla > 769){
     new Glide('.glide', {
         type: 'carousel',
         perView: 3,
