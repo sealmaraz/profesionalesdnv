ccd93acba2a39fcd6658fdf98cde0b7747ebf0a6
Elizabeth Almaraz <ealmaraz@correo.com>
2024-03-08 15:05:46 -0300

    [FEAT] rearmado de carousel

---
+++ pages/carousel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/pages/carousel.css">
+</head>
+<body>
+    
+</body>
+</html>
+<section class="containerCarousel">
+    <article class="carouselGeneral">
+        <img class="carouselGeneral_img" src="../img/carousel1/img_01.jpeg" alt="sorteo01">
+        <img class="carouselGeneral_img" src="../img/carousel1/img_02.jpeg" alt="sorteo02">
+        <img class="carouselGeneral_img" src="../img/carousel1/img_03.jpeg" alt="sorteo03">
+        <img class="carouselGeneral_img" src="../img/carousel1/img_04.jpeg" alt="sorteo04">
+        <img class="carouselGeneral_img" src="../img/carousel1/img_05.jpeg" alt="sorteo05">
+    </article>
+</section>
