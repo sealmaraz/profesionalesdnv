a030ea8dcd69867c0f9d721957b4b4b93a76719f
Elizabeth Almaraz <ealmaraz@correo.com>
2024-03-11 15:05:25 -0300

    [FEAT] rearmado de carousel

--- pages/carousel.php
+++ pages/carousel.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/pages/carousel.css">
-</head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 <body>
     
 </body>
 </html>
 <section class="containerCarousel">
+    <i class="fa-solid  fa-angle-left"></i>
     <article class="carouselGeneral">
         <img class="carouselGeneral_img" src="../img/carousel1/img_01.jpeg" alt="sorteo01">
         <img class="carouselGeneral_img" src="../img/carousel1/img_02.jpeg" alt="sorteo02">
@@ -18,4 +19,5 @@
         <img class="carouselGeneral_img" src="../img/carousel1/img_04.jpeg" alt="sorteo04">
         <img class="carouselGeneral_img" src="../img/carousel1/img_05.jpeg" alt="sorteo05">
     </article>
+    <i class="fa-solid  fa-angle-right"></i>
 </section>
